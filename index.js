8693c6e66c0ba3f30c5c4633bfdd9855edf3fd63
khushii1 <144243023+khushii1@users.noreply.github.com>
2023-09-18 21:08:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import mongoose, { mongo } from 'mongoose'
 import cors from 'cors'
 import useRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser'                         
 import answerRoutes from "./routes/Answers.js";
 import dotenv from "dotenv";
+import plans from "./models/plan.js";
+import Questions from './models/Questions.js';
 dotenv.config();
 
 const app= express();
@@ -27,6 +29,97 @@ app.get('/',(req,res)=>{
 app.use("/user",useRoutes)
 app.use("/questions",questionRoutes)
 app.use("/answer", answerRoutes);
+app.patch("/planupdate",async(req, res) => {
+  const plan = req.body;
+  const userId = req.userId;
+  const email = req.body.email;
+
+      
+ 
+  
+
+
+
+ const postQuestion = new plans({ ...plan, userId });
+  try {
+    const existinguser = await plans.findOne({ email });
+    if (existinguser) {
+      plans.deleteOne({ email : email });
+      res.status(200).json("Deleted successfully");
+    }
+    else{
+    await postQuestion.save();
+    res.status(200).json("Posted a question successfully");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(409).json("Couldn't post a new question");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  console.log(req.body.plan);
+console.log(req.body);
+           const updatedocu= (id)=>{
+
+
+
+
+/* users.findByIdAndUpdate(id, { plan: plan.plan },
+            function (err, docs) {
+if (err){
+console.log(err)
+}
+else{
+console.log("Updated User : ", docs);
+}
+});
+
+   /*    const result=await users.updateOne({id : id},{
+        $set:{
+          plan : req.body.plan
+        }
+       });
+       console.log(result);*/
+           }
+
+           updatedocu(userId);
+})
+
+app.post('/getplan',async(req, res) =>{
+
+//  const plan = req.body;
+  const userId = req.body.userId;
+  const email = req.body.email;
+  const name = req.body.name;
+  const existinguser = await plans.findOne({email});
+  
+  
+
+  //console.log(req.body)
+
+  const countsquestions = await Questions.find({userPosted : name});
+
+   const val = countsquestions;
+
+
+  res.status(200).json({ plan: existinguser , counts : countsquestions , id:userId});
+
+
+
+
+})
+
+
 
 
 const PORT= 5000
